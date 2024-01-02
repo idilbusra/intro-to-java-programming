24662182d7fb1fef18ea8e86d3ba7b23f9cefb35
busra idil <diyetisyenbusraidil@gmail.com>
2024-01-02 13:10:09 +0300

    Variables

---
+++ src/main/java/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        int ogrenciSayisi =12;
+        System.out.println("Ogrenci sayim: "+ ogrenciSayisi);
+        System.out.println("Ogrenci sayim: "+ ogrenciSayisi);
+        System.out.println("Ogrenci sayim: "+ ogrenciSayisi);
+        System.out.println("Ogrenci sayim: "+ ogrenciSayisi);
+        System.out.println("Ogrenci sayim: "+ ogrenciSayisi);
+    }
+}
