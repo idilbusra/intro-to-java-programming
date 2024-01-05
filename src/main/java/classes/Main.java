738639bb6410edacdbed495a15759621d76fcf2a
busra idil <diyetisyenbusraidil@gmail.com>
2024-01-05 22:03:32 +0300

    CustomerManager2

--- src/main/java/classes/Main.java
+++ src/main/java/classes/Main.java
@@ -2,6 +2,12 @@ package classes;
 
 public class Main {
     public static void main(String[] args) {
-
+        //reference type
+        CustomerManager customerManager = new CustomerManager();
+        CustomerManager customerManager2= new CustomerManager();
+        customerManager=customerManager2;
+        customerManager.Add();
+        customerManager.Remove();
+        customerManager.Update();
     }
 }
