e4d0b179216bc4edcb21b32a9a9aa22a5d36c02c
busra idil <diyetisyenbusraidil@gmail.com>
2024-01-06 20:51:19 +0300

    Getter and Setter

--- src/main/java/classeswithattributes/Main.java
+++ src/main/java/classeswithattributes/Main.java
@@ -3,15 +3,15 @@ package classeswithattributes;
 public class Main {
     public static void main(String[] args) {
         Product product = new Product();
-      //  product.name= "Laptop";
-      //  product.id=1;
-      //  product.description="Asus Laptop";
-      //  product.price=5000;
-      //  product.stockAmount=3;
+        product.setName("Laptop");
+        product.setId(1);
+        product.setDescriptions("Asus Laptop");
+        product.setPrice(5000);
+        product.setStockAmount(3);
 
         ProductManager productManager = new ProductManager();
-      //  productManager.Add(product);
-     //   System.out.println(product.name);
+        productManager.Add(product);
+        System.out.println(product.getKod());
 
 
     }
--- src/main/java/classeswithattributes/Product.java
+++ src/main/java/classeswithattributes/Product.java
@@ -4,16 +4,66 @@ public class Product {
 
     //attribute | field
     private int id;
-    private String name;
-    private String description;
-    private double price;
-    private int stockAmount;
-    private String renk;
-    private String kod;
+     String name;
+     String descriptions;
+     double price;
+     int stockAmount;
+     String renk;
+     String kod;
 
-    public void metot(){
-
+    public int getId() {
+        return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescriptions() {
+        return descriptions;
+    }
+
+    public void setDescriptions(String descriptions) {
+        this.descriptions = descriptions;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockAmount() {
+        return stockAmount;
+    }
+
+    public void setStockAmount(int stockAmount) {
+        this.stockAmount = stockAmount;
+    }
+
+    public String getRenk() {
+        return renk;
+    }
+
+    public void setRenk(String renk) {
+        this.renk = renk;
+    }
+
+    public String getKod() {
+        return this.name.substring(0,1)+ id;
+    }
+
+    public void setKod(String kod) {
+        this.kod = kod;
+    }
 }
