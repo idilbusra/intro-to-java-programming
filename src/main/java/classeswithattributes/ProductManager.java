855819760fa2b5be61a0327c7a68dfb107395163
busra idil <diyetisyenbusraidil@gmail.com>
2024-01-06 20:12:22 +0300

    Encapsulation1

---
+++ src/main/java/classeswithattributes/ProductManager.java
@@ -0,0 +1,8 @@
+package classeswithattributes;
+
+public class ProductManager {
+    public void Add(Product product){
+        //JDBC
+        System.out.println("Urun eklendi "+ product.name );
+    }
+}
