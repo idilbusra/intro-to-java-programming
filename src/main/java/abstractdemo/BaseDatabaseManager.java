7f17890fc4aa3db50e0d4c694d4c4752a529578d
busra idil <diyetisyenbusraidil@gmail.com>
2024-01-29 23:22:14 +0300

    AbstractDemo

---
+++ src/main/java/abstractdemo/BaseDatabaseManager.java
@@ -0,0 +1,6 @@
+package abstractdemo;
+
+public abstract class BaseDatabaseManager {
+    public abstract void getData();
+
+}
---
+++ src/main/java/abstractdemo/OracleDatabaseManager.java
@@ -0,0 +1,9 @@
+package abstractdemo;
+
+public class OracleDatabaseManager extends  BaseDatabaseManager{
+    @Override
+    public void getData() {
+        System.out.println("Veri getirildi: Oracle ");
+
+    }
+}
