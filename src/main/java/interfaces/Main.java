f8c2709a805ba9ea695d31f6402a7815c0394e36
busra idil <diyetisyenbusraidil@gmail.com>
2024-01-29 23:54:15 +0300

    Interface2

--- src/main/java/interfaces/Main.java
+++ src/main/java/interfaces/Main.java
@@ -2,6 +2,8 @@ package interfaces;
 
 public class Main {
     public static void main(String[] args) {
-        ICustomerDal customerDal = new OracleCustomerDal();
+        CustomerManager customerManager = new CustomerManager(new OracleCustomerDal());
+
+        customerManager.add();
     }
 }
