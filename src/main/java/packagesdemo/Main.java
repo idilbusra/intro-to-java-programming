36041f57f9ef099387370d69637fa2db53f8df6b
busra idil <diyetisyenbusraidil@gmail.com>
2024-01-31 23:03:37 +0300

    Packages2

--- src/main/java/packagesdemo/Main.java
+++ src/main/java/packagesdemo/Main.java
@@ -1,4 +1,9 @@
 package packagesdemo;
+import packagesdemo.matematik.DortIslem;
+import packagesdemo.matematik.Logaritma;
+
+import packagesdemo.matematik.*;
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
@@ -8,5 +13,12 @@ public class Main {
 
         String isim = scanner.nextLine();
         System.out.println("Merhaba "+isim);
+
+
+        DortIslem dortIslem = new DortIslem();
+        dortIslem.topla(2,3);
+
+        Logaritma logaritma= new Logaritma();
+
     }
 }
