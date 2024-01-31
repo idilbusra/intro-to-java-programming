a9b864b2988b6bec609838153851eb7beae4a628
busra idil <diyetisyenbusraidil@gmail.com>
2024-01-31 23:48:41 +0300

    ArrayListWithClass

---
+++ src/main/java/arraylistwithclass/Main.java
@@ -0,0 +1,23 @@
+package arraylistwithclass;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Customer> customers = new ArrayList<Customer>();
+        customers.add(new Customer(1,"Engin", "Demirog"));
+        customers.add(new Customer(2,"Derin", "Demirog"));
+        customers.add(new Customer(3,"Salih", "Demirog"));
+
+        customers.remove((new Customer(1,"Engin", "Demirog")));//silinmez eklenir
+
+        for (Customer customer: customers){
+            System.out.println(customer.firstName);
+        }
+
+
+
+    }
+
+
+}
