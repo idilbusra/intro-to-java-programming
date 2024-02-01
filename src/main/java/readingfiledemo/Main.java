04b624f3c07f6041c07507b8072215088c60ff1a
busra idil <diyetisyenbusraidil@gmail.com>
2024-02-01 23:17:56 +0300

    Checked Exceptions

--- src/main/java/readingfiledemo/Main.java
+++ src/main/java/readingfiledemo/Main.java
@@ -3,14 +3,22 @@ package readingfiledemo;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
+
 
 public class Main {
     public static void main(String[] args) {
         BufferedReader reader = null;
+        int total=0;
         try {
             reader = new BufferedReader(new FileReader("C:\\Users\\idil_\\IdeaProjects\\javademos\\src\\main\\java\\readingfiledemo\\sayilar.txt"));
+            String line= null;
+            while((line=reader.readLine())!=null){
+                total += Integer.valueOf(line);
+            }
+            System.out.println("Toplam =" + total);
         } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         } finally {
             try {
