84b0c411814a46bede5ad74f4806de7e94f9d6a4
busra idil <diyetisyenbusraidil@gmail.com>
2024-02-01 22:12:56 +0300

    Demo Exception

--- src/main/java/exceptionhandling/Main.java
+++ src/main/java/exceptionhandling/Main.java
@@ -1,14 +1,18 @@
 package exceptionhandling;
 
 public class Main {
-    public static void main (String arg[]){
+    public static void main (String arg[]) {
 
         try {
-            int[] sayilar = new int[]{1,2,3};
+            int[] sayilar = new int[]{1, 2, 3};
             System.out.println(sayilar[5]);
-        }catch (Exception exception){
+        } catch (StringIndexOutOfBoundsException exception) {
             System.out.println("Hata olustu");
-        }finally {
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            System.out.println("Hata olustu");
+        }catch (Exception exception){
+            System.out.println("Loglandi: "+ exception);
+        }finally{
             System.out.println("Ben her turlu calisirim");
         }
     }
