7e4585e78bf82b5689af18ab512df7da9c208e6c
busra idil <diyetisyenbusraidil@gmail.com>
2024-02-01 23:47:48 +0300

    ThrowDemo

---
+++ src/main/java/throwdemo/AccountManager.java
@@ -0,0 +1,21 @@
+package throwdemo;
+
+public class AccountManager {
+    private double balance;
+
+    public void deposit(double amount){
+        balance = getBalance() + amount;
+    }
+
+    public void withdraw(double amount) throws Exception {
+        if (balance>=amount){
+            balance= getBalance()- amount;
+        }else{
+            throw new Exception("Bakiye yetersiz");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
