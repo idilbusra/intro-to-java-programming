5af08d15ab38aa2bd4e72f08b2554803d9b8bb96
busra idil <diyetisyenbusraidil@gmail.com>
2024-02-03 21:24:28 +0300

    GetFileInfo

--- src/main/java/workingwithfiles/Main.java
+++ src/main/java/workingwithfiles/Main.java
@@ -5,17 +5,32 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        createFile();
+        getFileInfo();
+    }
+    public static void createFile(){
         File file= new File("C:\\Users\\idil_\\IdeaProjects\\javademos\\src\\main\\java\\files\\students.txt");
         try {
-           if(file.createNewFile()){
-               System.out.println("Dosya olusturuldu");
-           }else{
-               System.out.println("Dosya zaten mevcut");
-           }
+            if(file.createNewFile()){
+                System.out.println("Dosya olusturuldu");
+            }else{
+                System.out.println("Dosya zaten mevcut");
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
+    public static  void getFileInfo(){
+        File file= new File("C:\\Users\\idil_\\IdeaProjects\\javademos\\src\\main\\java\\files\\students.txt");
+        if (file.exists()){
+            System.out.println("Dosya adi "+ file.getName());
+            System.out.println("Dosya yolu "+ file.getAbsoluteFile());
+            System.out.println("Dosya yazilabilir mi: "+ file.canWrite());
+            System.out.println("Dosya okunabilir mi : "+ file.canRead());
+            System.out.println("Dosya boyutu (byte) : "+ file.length());
+        }
+    }
+
+
 
 }
